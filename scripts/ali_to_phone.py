b0047861c4eae8718015061cb9be7a4b110e2926
tjysdsg <tjy1018543509@163.com>
2020-12-22 17:10:06 +0800

    Update aligning script

--- scripts/ali_to_phone.py
+++ scripts/ali_to_phone.py
@@ -6,15 +6,15 @@ phone_dir = sys.argv[1]
 ctm = sys.argv[2]
 phone_ctm = sys.argv[3]
 
-dict_phones = dict({item.split()[1]:item.split()[0] for item in open(phone_dir)})
+dict_phones = dict({item.split()[1]: item.split()[0] for item in open(phone_dir)})
 
 f1 = open(phone_ctm,"w")
 
 
 for item in open(ctm):
     data = item.strip().split()
-    #print(dict_phones)
-    data[-1] = dict_phones[data[-1]].split("_")[0]
+    # print(dict_phones)
+    data[-1] = dict_phones[data[-1]]
     f1.writelines(" ".join(data) + "\n")
 f1.close()
 
