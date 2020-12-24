9c5e8b4b00c2e84cd92cfb2ba122a69edf21bcac
tjysdsg <tjy1018543509@163.com>
2020-12-24 22:06:07 +0800

    Fix a stupid mistake

--- scripts/show_phone_alignment.sh
+++ scripts/show_phone_alignment.sh
@@ -1,19 +1,25 @@
 . path.sh
 
-ali_path=exp/tri5a_ali
-stage=2
-for f in $ali_path/ali.*.gz; do
-    if [ $stage -le 1 ]; then
-        echo $f
-        filename="${f##*/}"
-        alignfile=$ali_path/$filename.ali
-        zcat $f > $alignfile
-        mkdir -p alignments
-        ali-to-phones --ctm-output $ali_path/final.mdl ark:$alignfile alignments/$filename.ctm
-    fi
+ali_path=/NASdata/pc_backup/jiayan/close_talk_manderin/exp/tri14_sp_ali
+stage=1
 
-    if [ $stage -le 2 ]; then
-        cat alignments/*ctm > phone_ctm.ctm
-        python ali_to_phone.py $ali_path/phones.txt phone_ctm.ctm phone_ctm.txt
-    fi
-done
+mkdir -p alignments
+if [ $stage -le 1 ]; then
+    for f in $ali_path/ali.*.gz; do
+        filename="${f##*/}"
+        ctmfile=alignments/$filename.ctm
+        if [ ! -f $ctmfile ]; then
+            echo "$f -> $ctmfile"
+            alignfile=$ali_path/$filename.ali
+            zcat $f > $alignfile
+            ali-to-phones --ctm-output $ali_path/final.mdl ark:$alignfile $ctmfile
+        else
+            echo "Skipping $f because $ctmfile already exists"
+        fi
+    done
+fi
+
+if [ $stage -le 2 ]; then
+    cat alignments/*ctm > phone_ctm.ctm
+    python ali_to_phone.py $ali_path/phones.txt phone_ctm.ctm phone_ctm.txt
+fi
